bb665c0433eb746edc9102d3aecb8ccfe3dfdec7
ericx790101 <ericx790101@gmail.com>
2022-04-22 22:32:11 +0800

    feat: 完成實作密碼產生器

---
+++ generate_password.js
@@ -0,0 +1,67 @@
+// define sample function to randomly return a item in an array
+function sample(array) {
+  const index = Math.floor(Math.random() * array.length)
+  return array[index]
+}
+
+// define generatePassword function
+function generatePassword(options) {
+  // define things user might want
+  const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+  const upperCaseLetters = lowerCaseLetters.toUpperCase()
+  const numbers = '1234567890'
+  const symbols = '`~!@$%^&*()-_+={}[]|;:"<>,.?/'
+
+  // // dummy data
+  // const options = {
+  // length: '12',
+  // lowercase: 'on',
+  // uppercase: 'on',
+  // numbers: 'on',
+  // symbols: 'on',
+  // excludeCharacters: '40'
+  // }
+
+
+  // create a collection to store things user picked up
+  let collection = []
+  if (options.lowercase === 'on') {
+    collection = collection.concat(lowerCaseLetters.split(''))
+  }
+
+  if (options.uppercase === 'on') {
+    collection = collection.concat(upperCaseLetters.split(''))
+  }
+
+  if (options.numbers === 'on') {
+    collection = collection.concat(numbers.split(''))
+  }
+
+  if (options.symbols === 'on') {
+    collection = collection.concat(symbols.split(''))
+  }
+
+  // remove things user do not need
+  if (options.excludeCharacters) {
+    collection = collection.filter(
+      character => !options.excludeCharacters.includes(character)
+    )
+  }
+
+  // return error notice if collection is empty
+  if (collection.length === 0) {
+    return 'There is no valid character in your selection.'
+  }
+  
+  // start generating password
+  let password = ''
+  for (let i= 0; i < Number(options.length); i++) {
+    password += sample(collection)
+  }
+  // return the generated password
+  return password
+}
+
+// generatePassword()
+
+module.exports = generatePassword
